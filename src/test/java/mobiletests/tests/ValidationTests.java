2c7be1740e948e2e91cbe7590c107442e8a0703e
Pasha <kramarpavlo@gmail.com>
2024-12-18 12:08:32 +0200

    framework 1,2,3

--- src/test/java/mobiletests/tests/ValidationTests.java
+++ src/test/java/mobiletests/tests/ValidationTests.java
@@ -3,14 +3,12 @@ package mobiletests.tests;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import io.appium.java_client.AppiumBy;
 
 import java.net.URL;
-import java.time.Duration;
 
 public class ValidationTests {
     private IOSDriver driver;
@@ -35,12 +33,11 @@ public class ValidationTests {
         By submitButton = AppiumBy.accessibilityId("submit_button");
         By errorLabel = AppiumBy.accessibilityId("error_label");
 
-        int invalidValue = 1_000_002;
+        int invalidValue = 1000001;
 
         driver.findElement(inputField).sendKeys(String.valueOf(invalidValue));
         driver.findElement(submitButton).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         String errorText = driver.findElement(errorLabel).getText();
 
         assert errorText.contains("exceeds maximum") :
